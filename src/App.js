87f1903f4512c1dff24c8ce98bcce0cd5c4a0526
Daisy <chisom@daisy-micheal.lan>
2024-02-20 11:12:03 +0100

    Add Tailwind CSS and Bootstrap for styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './Components/Header/Header'
+import Page from './Components/Pages/Home'
+import Style from './Components/Style /style.css'
+import Trend from './Components/Pages/Trend'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header />
+      <Page />
+      <Trend />
+    </>
+  )
 }
 
-export default App;
+export default App
